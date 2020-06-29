f005af0578c2b906e4e496c1f3263c9b2639391c
KillerDucks <nimesh.patel@bindserver.com>
2020-06-29 09:15:27 -0700

    Allow building of diffrent containers via opts

--- makeDocker_env.sh
+++ makeDocker_env.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "Building the Docker Image for WebXServer Development Enviorment"
-docker build . -t webxserver-env:latest
+# docker build --rm . -t webxserver-env:latest
+while getopts fte opt; do
+    case $opt in
+        f) docker build --rm . -t webxserver-env:latest
+        ;;
+        t) docker build --rm --file Dockerfile.tiny -t webxserver-tiny:latest .
+        ;;
+        e) docker build --rm --file Dockerfile.exec -t webxserver-exec:latest .
+        ;;
+    esac
+done
