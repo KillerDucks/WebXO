3a35f24983433b99d9fe8df149d24fb59b84b984
[melissa@melissaxo.xyz] <melissa@.localdomain>
2020-09-18 11:54:32 +0100

    Updated version

--- src/WebXLib/Common.hpp
+++ src/WebXLib/Common.hpp
@@ -12,7 +12,7 @@
 constexpr char* HTTP_VERSION    = (char*)"HTTP/1.1";
 constexpr char* SERVER_STRING   = (char*)"Server: WebXO/1.2.1";
 
-constexpr char* ERROR_PAGE_DIR   = (char*)"./ErrorPages";
+constexpr char* ERROR_PAGE_DIR   = (char*)"/var/www/ErrorPages";
 
 // Typedefs to save time
 typedef std::pair<char*, int> CompBuffer;
