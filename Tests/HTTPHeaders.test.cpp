575da0bf1fe69367c805cf45c9ad3941b3265832
KillerDucks <nimesh.patel@bindserver.com>
2020-10-06 14:08:59 +0100

    Added tests for HTTP Header generation

---
+++ Tests/HTTPHeaders.test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+// Include in the Class to test
+#include "../src/WebXLib/HTTPHeaders.hpp"
+
+// Tests the basic core HTTP Header Generation
+TEST(HTTPHeaders, General_Header) {
+    using namespace WebXO;
+    HGENERAL gHeader;
+    gHeader.Date = gHeader.GetCurrentTime();
+    EXPECT_EQ("Connection: close", gHeader.Connection);
+}
+
+// Tests the basic core HTTP Entity Generation
+TEST(HTTPHeaders, Entity_Header) {
+    using namespace WebXO;
+    hEntity eHeader;    
+    EXPECT_EQ("Allow: GET, POST\r\nContent-Type: text/html;\r\nContent-Language: en-GB, en-US\r\n\r\n", eHeader.ReturnHeader());
+}
+
+// [TODO] Tests the basic HTTP Reponse Header Generation
