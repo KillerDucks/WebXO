b43656ef06fa04ca05521df098a193db1a5d4ca6
KillerDucks <nimesh.patel@bindserver.com>
2020-10-10 00:36:02 +0100

    Removed debug code

--- Tests/VirtualHosts.test.cpp
+++ Tests/VirtualHosts.test.cpp
@@ -1,15 +1,12 @@
 #include <gtest/gtest.h>
 
-#include <unistd.h>
+// #include <unistd.h>
 
 // Include in the Class to test
 #include "../src/WebXLib/VirtualHosts.hpp"
 
 // Tests Parsing and Querying of a vhost.txt file
 TEST(VirtualHosts, Parse) {
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    printf("Current Working Directory: %s\n", cwd);
     WebXO::VirtualHosts vhost("./assets/vhosts.txt"); 
     EXPECT_EQ(std::string("/GTest"), vhost.Query("GTest.localhost"));
 }
