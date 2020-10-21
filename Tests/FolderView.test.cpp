fce7cfb2529a6e1bd6be9ad8db02eab8816e7adf
KillerDucks <nimesh.patel@bindserver.com>
2020-10-21 23:48:55 +0100

    Fixed test for page generation

--- Tests/FolderView.test.cpp
+++ Tests/FolderView.test.cpp
@@ -3,14 +3,16 @@
 // Include in the Class to test
 #include "../src/WebXLib/FolderView.hpp"
 
+// This test will see if all the files are retuned
 TEST(FolderView, GetFiles) {
     using namespace WebXO;
     FolderView fViewer;
     EXPECT_EQ(3, fViewer.GetFiles(std::string("./assets")).size());
 }
 
+// This test will check to see if the page is getting generated (sort of)
 TEST(FolderView, GeneratePage) {
     using namespace WebXO;
     FolderView fViewer;
-    EXPECT_EQ(3, fViewer.GetFiles(std::string("./assets")).size());
+    EXPECT_EQ(13, fViewer.GeneratePage( fViewer.GetFiles( std::string("./assets") ) ).find_first_of('<') );
 }
