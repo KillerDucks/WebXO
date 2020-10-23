b04ccd770f9971566ed9f82355018cb848e7bd67
KillerDucks <nimesh.patel@bindserver.com>
2020-10-23 23:03:46 +0100

    Made Interception a header only file

--- src/WebXLib/Interception.hpp
+++ src/WebXLib/Interception.hpp
@@ -4,10 +4,10 @@
 
 #include <functional>
 
-#include "Logarithm.hpp"
-#include "HTTPMethodTypes.hpp"
-#include "HTTPHeaders.hpp"
 #include "Common.hpp"
+#include "Logarithm.hpp"
+#include "HTTPHeaders.hpp"
+#include "HTTPMethodTypes.hpp"
 
 #ifndef WEBX_INTERCEPTION_H
 #define WEBX_INTERCEPTION_H
@@ -16,7 +16,7 @@ namespace WebXO
 {
     // Interception Settings [TODO] Move into a separate file after testing
     typedef struct InterceptSettings{
-        InterceptSettings():isBlocking(false), method(HTTPMethodTypes::GET), 
+        InterceptSettings():method(HTTPMethodTypes::GET), 
                             callback([=](HTTPReq req) -> CompBuffer 
                             { 
                                 // This is just a default interception that will do nothing
@@ -37,29 +37,21 @@ namespace WebXO
         // Functions
         void ErrorHandler();
 
-        // Data
-        bool isBlocking;            // Make the interception block the request
-
         // Logger
         Logarithm _Log;
     public:
         // De/Constructors
-        Interception(InterceptSettings interceptSettings);                                      // Single Method to catch
-        Interception(HTTPMethodTypes method = HTTPMethodTypes::GET, bool isBlocking = false);   // Single Method to catch
-        // Interception(std::vector<HTTPMethodTypes> methods , bool isBlocking = false);           // Multiple Methods to catch
+        Interception(InterceptSettings interceptSettings)                                           // Single Method to catch
+        {};                                      
+        Interception(HTTPMethodTypes method = HTTPMethodTypes::GET)                                 // Single Method to catch
+        {};
+        // Interception(std::vector<HTTPMethodTypes> methods , bool isBlocking = false);            // Multiple Methods to catch
         ~Interception() = default;
 
         std::tuple<HTTPReq, CompBuffer> HookSync(HTTPReq info, std::function<CompBuffer(HTTPReq&)> func)
         {
             return std::tuple<HTTPReq, CompBuffer>(info, func(info));
         }
-
-        // [NOTE] Currently this is not async and will need to be altered
-        std::tuple<HTTPReq, CompBuffer> HookAsync(HTTPReq info, std::function<CompBuffer(HTTPReq&)> func)
-        {
-            return std::tuple<HTTPReq, CompBuffer>(info, func(info));
-        }
-
     };    
 }
 
