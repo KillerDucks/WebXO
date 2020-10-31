dcd0ae9d15378c43797d182e58507257cf631fa3
KillerDucks <nimesh.patel@bindserver.com>
2020-10-31 22:45:37 +0000

    Added version cli option and removed old code

--- src/pMain.cpp
+++ src/pMain.cpp
@@ -1,6 +1,7 @@
 // Includes
 #include "WebXLib/Sockets.hpp"
 #include "WebXLib/HTTP.hpp"
+#include "WebXLib/Common.hpp"
 
 struct Options 
 {
@@ -29,9 +30,6 @@ int main(int argc, char* argv[])
     WebXO::Sockets socks(http, optsCLI.port, sockSettings);
 
     // Start the Socket Server
-    // socks.Listen();
-
-    // [TODO] [CURRENT] [TESTING] Break this out into a thread so the CLI is still usable to perfrom admin activies
     std::thread th_serverThread(&WebXO::Sockets::Listen, std::ref(socks));
     th_serverThread.join();
 
@@ -54,7 +52,12 @@ Options ParseCLIOptions(std::vector<std::string> const vec)
         std::string tmpValue = opt.substr(opt.find("=")  + 1, opt.size());
 
         // Check if we have any options
-        if(opt == "--thread")
+        if(opt == "--version")
+        {
+            printf("Version: [%s]\n", SERVER_STRING);
+            exit(1);
+        }
+        else if(opt == "--thread")
         {
             optsCLI.threading = true;
         }
@@ -82,6 +85,8 @@ Options ParseCLIOptions(std::vector<std::string> const vec)
             printf("\t\tIf set this will see the root path to server HTTP requests from\n");
             printf("\n\t--port\n");
             printf("\t\tSets the port of the server to listen for clients on, (Default: 8080)\n");
+            printf("\n\t--version\n");
+            printf("\t\tShows the version info\n");
             printf("\n\t--help\n");
             printf("\t\tShows this message :P\n");
             exit(1);
