e3e46afcb5b7d8c8e9817217a13f7693e4dfbf37
KillerDucks <nimesh.patel@bindserver.com>
2020-10-31 15:51:32 +0000

    Updated test to namespace

--- Tests/Compression.test.cpp
+++ Tests/Compression.test.cpp
@@ -2,42 +2,24 @@
 
 // Include in the Class to test
 #include "../src/WebXLib/Compression.hpp"
+#include "../src/WebXLib/IO.hpp"
 
 // Tests the compression of a file [SIZE]
 TEST(Compression, DeflateFile_Size) {
-    WebXO::Compression compresstionTest;
-    CompBuffer testBuffer = compresstionTest.DeflateFile("./assets/deflateME.txt");
+    CompBuffer testBuffer = WebXO::Compression::DeflateFile("./assets/deflateME.txt");
     EXPECT_EQ(33, testBuffer.second);
 }
 
 // Tests the compression of a file [BUFFER]
-TEST(Compression, DeflateFile_CmpFile) {
-    WebXO::Compression compresstionTest;
-    CompBuffer testBuffer = compresstionTest.DeflateFile("./assets/deflateME.txt");
-
-    char* sBuffer;
-    int fLength = 0;
-    std::fstream fss;
-    fss.open("./assets/deflateME.txt.zz");
-    if(fss.good())
-    {
-        // File is good
-        fss.seekg(0, fss.end);
-        fLength = fss.tellg();
-        fss.seekg(0, fss.beg); 
-            
-        sBuffer = new char[fLength];
-        fss.read(sBuffer, fLength);
-
-        fss.close();
-    }
-    EXPECT_EQ(fLength, testBuffer.second);
+TEST(Compression, DeflateFile_CmpFile) {   
+    CompBuffer testBuffer = WebXO::Compression::DeflateFile("./assets/deflateME.txt");
+    CompBuffer compressedFile = WebXO::IO::ReadFile("./assets/deflateME.txt.zz");
+    EXPECT_EQ(compressedFile.second, testBuffer.second);
 }
 
 // Tests the compression of a Buffer
 TEST(Compression, DelfateBuffer) {
-    std::string sBuffer("THIS IS A TEST STRING BUFFER");
-    WebXO::Compression compresstionTest;
-    CompBuffer testBuffer = compresstionTest.DeflateBuffer((char*)sBuffer.c_str(), sBuffer.length());
+    std::string sBuffer("THIS IS A TEST STRING BUFFER");    
+    CompBuffer testBuffer = WebXO::Compression::DeflateBuffer((char*)sBuffer.c_str(), sBuffer.length());
     EXPECT_EQ(34, testBuffer.second);
 }
