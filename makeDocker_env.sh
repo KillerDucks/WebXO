4a5694ea0729e51d8ce87673ef127cb764a94cc2
[Melissa XO] <[melissa@melissaxo.xyz]>
2021-03-20 21:43:37 +0000

    Renamed docker build images

--- makeDocker_env.sh
+++ makeDocker_env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Building the Docker Image for WebXServer Development Enviorment"
-# docker build --rm . -t webxserver-env:latest
+echo "Building the Docker Image for webxo Development Enviorment"
+# docker build --rm . -t webxo-env:latest
 while getopts fte opt; do
     case $opt in
-        f) docker build --rm . -t webxserver-env:latest
+        f) docker build --rm . -t webxo-env:dev
         ;;
-        t) docker build --rm --file Dockerfile.tiny -t webxserver-tiny:latest .
+        t) docker build --rm --file Dockerfile.tiny -t webxo-tiny:dev .
         ;;
-        e) docker build --rm --file Dockerfile.exec -t webxserver-exec:latest .
+        e) docker build --rm --file Dockerfile.exec -t webxo-exec:dev .
         ;;
     esac
 done
