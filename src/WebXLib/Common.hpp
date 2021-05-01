98d21b69b69f7e5a625323bb6feebe03baae1b24
[Melissa XO] <[melissa@melissaxo.xyz]>
2021-05-01 19:19:28 +0100

    Updated the version string

--- src/WebXLib/Common.hpp
+++ src/WebXLib/Common.hpp
@@ -13,7 +13,7 @@
 
 // Whatever this stuff is O.o
 constexpr char* HTTP_VERSION        = (char*)"HTTP/1.1";
-constexpr char* SERVER_STRING       = (char*)"Server: WebXO/1.6.0";
+constexpr char* SERVER_STRING       = (char*)"Server: WebXO/1.6.5";
 constexpr char* AUTHOR_STRING       = (char*)"MelissaXO<melissa@melissaxo.xyz>";
 
 constexpr char* ERROR_PAGE_DIR      = (char*)"/var/www/ErrorPages";
