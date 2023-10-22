9588da4578b587944b96295cafa7a7ba1e07ae75
Melissa <melissa@hyperlxc.co.uk>
2023-10-22 17:47:32 +0100

    Updated codename and version number

--- src/WebXLib/Common.hpp
+++ src/WebXLib/Common.hpp
@@ -13,8 +13,8 @@
 
 // Whatever this stuff is O.o
 constexpr char* HTTP_VERSION        = (char*)"HTTP/1.1";
-constexpr char* SERVER_STRING       = (char*)"Server: WebXO/1.6.5";
-constexpr char* AUTHOR_STRING       = (char*)"MelissaXO<melissa@melissaxo.xyz>";
+constexpr char* SERVER_STRING       = (char*)"Server: WebXO/1.7.0";
+constexpr char* AUTHOR_STRING       = (char*)"melissa<melissa@noreply-hyperlxc.co.uk>";
 
 constexpr char* ERROR_PAGE_DIR      = (char*)"/var/www/ErrorPages";
 
